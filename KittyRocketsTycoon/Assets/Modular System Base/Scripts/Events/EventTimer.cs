05c622656330abb899fa8de854caa606c4af7f81
Olmer "Penny" Chavez-Ramirez <32857108+PennysBrain@users.noreply.github.com>
2018-08-15 10:32:59 -0500

    Initial commit

---
+++ KittyRocketsTycoon/Assets/Modular System Base/Scripts/Events/EventTimer.cs
@@ -0,0 +1,50 @@
+﻿using WARMachine.Variables;
+using UnityEngine;
+
+namespace WARMachine.Events
+{
+    public class EventTimer : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [Multiline]
+        public string DeveloperDescription = "";
+#endif
+
+        [Tooltip("Event To Raise")]
+        public GameEvent Event;
+
+        public bool ActiveatStart;
+        public FloatReference ResetTimer;
+        public float currentTime;
+        public bool saveCurrentTime = false;
+        public FloatReference CurrentTimerSave;
+        // Use this for initialization
+        void Start()
+        {
+            if (ActiveatStart)
+            {
+                ActivateEvent();
+            }
+        }
+
+        void Update()
+        {
+            currentTime -= Time.deltaTime;
+            if (saveCurrentTime)
+            {
+                CurrentTimerSave.Variable.Value = currentTime;
+            }
+            if (currentTime < 0)
+            {
+                ActivateEvent();
+                Debug.Log("THIS HAPPPENS");
+                currentTime = ResetTimer;
+            }
+        }
+
+        void ActivateEvent()
+        {
+            Event.Raise();
+        }
+    }
+}
