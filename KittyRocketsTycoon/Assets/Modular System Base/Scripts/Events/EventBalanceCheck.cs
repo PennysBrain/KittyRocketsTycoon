832bd649b8bef8f42a8e8e1dc584726481a9f97f
Olmer "Penny" Chavez-Ramirez <32857108+PennysBrain@users.noreply.github.com>
2018-08-16 13:06:21 -0500

    Balance Check Exacute Event
    Balance Checks if A >= B Executes Event.
    Ship Value is Set in the buying process to keep the price and resource cost the same value where we can change them in the same place.

---
+++ KittyRocketsTycoon/Assets/Modular System Base/Scripts/Events/EventBalanceCheck.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WARMachine.Variables;
+
+namespace WARMachine.Events
+{
+    public class EventBalanceCheck : MonoBehaviour
+    {
+        [Tooltip("Event To Raise If A > B")]
+        public GameEvent Event;
+
+        public bool ActivateEvent = false;
+
+        public FloatReference A;
+        public FloatReference B;
+
+
+        // Use this for initialization
+       public void Activate()
+        {
+            if (A.Value >= B.Value)
+                Event.Raise();
+            else
+            {
+                Debug.Log("not enough " + A.Value.ToString() +" >= "+ B.Value.ToString());
+            }
+        }
+    }
+}
