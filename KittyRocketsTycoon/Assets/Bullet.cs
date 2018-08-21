65a3180b89a047ae48d58b4350c36a5fafd3dc67
Olmer "Penny" Chavez-Ramirez <32857108+PennysBrain@users.noreply.github.com>
2018-08-21 09:42:26 -0500

    Sectors Scene Created
    New sector Scan implemented
    Game Events Created for Scaning Started and Stop
    Scaning Varibales created for Current Scan Time and Scan Time Max
    Bullet Speed change faster
    Sector Prefab created
    Sector Scene Set up with own Manager Listing two events right now

---
+++ KittyRocketsTycoon/Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WARMachine.Variables;
+public class Bullet : MonoBehaviour
+{
+    public FloatReference Speed;
+    public FloatReference ShipPosition;
+    public float spawnXPosition;
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+
+    private void OnEnable()
+    {
+        spawnXPosition = ShipPosition.Value;
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.position = new Vector3(spawnXPosition, transform.position.y + Speed.Value * Time.deltaTime, transform.position.z);	
+	}
+}
--- KittyRocketsTycoon/Assets/Scripts/DragAndDrop.cs
+++ KittyRocketsTycoon/Assets/Scripts/DragAndDrop.cs
@@ -33,9 +33,9 @@ public class DragAndDrop : MonoBehaviour
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        Vector2 direction = new Vector2(
-            mousePosition.x - transform.position.x,
-            mousePosition.y - transform.position.y);
+//
+  //          mousePosition.x - transform.position.x,
+  //          mousePosition.y - transform.position.y);
 
         // this.transform.up = direction;//small rotation
         
