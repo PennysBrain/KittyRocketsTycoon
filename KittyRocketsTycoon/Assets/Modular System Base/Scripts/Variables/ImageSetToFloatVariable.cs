9063512f8339c0a1699f7832a80f048f16b0d0f1
Olmer "Penny" Chavez-Ramirez <32857108+PennysBrain@users.noreply.github.com>
2018-08-28 11:26:24 -0500

    Image Set to float varible Locked Unlocked
    UI updates to set locked unlocked Sprites in real time Update()
    Created Lockes for each planet
    Create Planet LockedUnlock Varibles one for each Sector
    UI unlocks self and lockes all others when clicked
    Prefab Created to share Button Varibles accross Lockes UI

---
+++ KittyRocketsTycoon/Assets/Modular System Base/Scripts/Variables/ImageSetToFloatVariable.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace WARMachine.Variables
+{
+    /// <summary>
+    /// Sets an Image component's to represent Variable
+    /// between One or the other.
+    /// </summary>
+    public class ImageSetToFloatVariable : MonoBehaviour
+    {
+        [Tooltip("Value to check if current Unlocked or Locked")]
+        public FloatVariable Variable;
+        [Tooltip("Image to set the fill amount on")]
+        public Image Image;
+
+        [Header("Locked Sprites")]
+        [Tooltip("Value to use as the Locked")]
+        public FloatReference VariableLocked;
+        public Sprite LockedSprite;
+
+        [Header("Unlocked Sprites")]
+        [Tooltip("Value to use as the Unlock")]
+        public FloatReference VariableUnlocked;  
+        public Sprite UnlockedSprite;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //FloatReference fl = VariableLocked;
+           // Debug.Log(Variable.Value + " == " + VariableLocked.Value);
+            if (Variable.Value == VariableLocked.Value)
+            {
+                Image.sprite = LockedSprite;
+            }
+            if (Variable.Value == VariableUnlocked.Value)
+            {
+                Image.sprite = UnlockedSprite;
+            }
+        }
+    }
+}
